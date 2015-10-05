d060747c249b922ab46bf906280a367e1efa9c5f
Florian Schmitt <florian@florianschmitt.de>
2015-10-05 20:27:38 +0200

    ExtTokenField demo-application

---
+++ ext-token-field-demo/src/main/java/ui/ServletInitializer.java
@@ -0,0 +1,14 @@
+package ui;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer
+{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(TestApp.class);
+	}
+}
---
+++ ext-token-field-demo/src/main/java/ui/TestUI.java
@@ -0,0 +1,271 @@
+/*
+ * Copyright 2015 Explicatis GmbH <ext-token-field@explicatis.com>
+ * 
+ * Author: Florian Schmitt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ui;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.explicatis.ext_token_field.ExtTokenField;
+import com.explicatis.ext_token_field.SimpleTokenizable;
+import com.explicatis.ext_token_field.Tokenizable;
+import com.explicatis.ext_token_field.TokenizableAction;
+import com.explicatis.ext_token_field.events.TokenAddedEvent;
+import com.explicatis.ext_token_field.events.TokenAddedListener;
+import com.explicatis.ext_token_field.events.TokenRemovedEvent;
+import com.explicatis.ext_token_field.events.TokenRemovedListener;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Widgetset;
+import com.vaadin.data.Item;
+import com.vaadin.data.Property.ValueChangeListener;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CheckBox;
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
+
+@SuppressWarnings("serial")
+@Theme(ValoTheme.THEME_NAME)
+@Widgetset(value = "widgetset.WidgetSet")
+@SpringUI
+public class TestUI extends UI
+{
+
+	private static final String	LABEL	= "label";
+	private VerticalLayout		mainLayout;
+
+	@Override
+	protected void init(VaadinRequest vaadinRequest)
+	{
+		mainLayout = new VerticalLayout();
+		mainLayout.setMargin(true);
+		mainLayout.setSpacing(true);
+
+		Label heading = new Label("ExtTokenField");
+		heading.addStyleName(ValoTheme.LABEL_HUGE);
+		Label subheading = new Label("2015 - Explicatis GmbH, Florian Schmitt");
+		subheading.addStyleName(ValoTheme.LABEL_TINY);
+
+		VerticalLayout head = new VerticalLayout(heading, subheading);
+
+		notes = new VerticalLayout();
+
+		addNote("Keyboard controls (arrow-left, arrow-right & delete)");
+		addNote("ComboBox or Button input (TextField support is planned)");
+		addNote("Custom actions can be defined, if the ignoreReadOnly.. setting is true, they will be still be usable, when the ExtTokenField is set to read only");
+		addNote("trims long token captions so that token is clickable to expose full caption (planned to be configurable, as to trimming length)");
+		addNote("implement <b>Tokenizable</b> interface in your bean or entity class to be able to set the fields value as a List of these objects");
+
+		notes.setSizeFull();
+		mainLayout.addComponent(head);
+		mainLayout.addComponent(notes);
+		mainLayout.addComponent(new ConfigurableLayout());
+		setContent(mainLayout);
+	}
+
+	private void addNote(String note)
+	{
+		Label l1 = new Label();
+		l1.setIcon(FontAwesome.CHECK_SQUARE);
+		Label l2 = new Label(note, ContentMode.HTML);
+		HorizontalLayout hl = new HorizontalLayout(l1, l2);
+		hl.setHeight(10, Unit.PIXELS);
+		hl.setSpacing(true);
+		notes.addComponent(hl);
+	}
+
+	private static String[]	LANGUAGES	= {"PHP", "Java", "JavaScript", "Scala", "Python", "C", "Ruby", "C++"};
+	private VerticalLayout	notes;
+
+	@SuppressWarnings("unchecked")
+	private static ComboBox getComboBox()
+	{
+		ComboBox b = new ComboBox();
+		b.setItemCaptionPropertyId(LABEL);
+		b.setInputPrompt("Type here to add");
+		b.addContainerProperty(LABEL, String.class, "");
+
+		for (String lang : LANGUAGES)
+		{
+			Object addItem = b.addItem();
+			b.getItem(addItem).getItemProperty(LABEL).setValue(lang);
+		}
+
+		return b;
+	}
+
+	private static Button getAddButton()
+	{
+		Button add = new Button();
+		add.setCaption("add element");
+		add.setIcon(FontAwesome.PLUS_CIRCLE);
+		add.addStyleName(ValoTheme.BUTTON_BORDERLESS);
+		add.addClickListener(event -> Notification.show("add clicked"));
+		return add;
+	}
+
+	private ValueChangeListener getComboBoxValueChange(ExtTokenField extTokenField, ComboBox comboBox)
+	{
+		return event -> {
+			Object id = event.getProperty().getValue();
+			if (id != null)
+			{
+				Item item = comboBox.getItem(id);
+				String string = (String) item.getItemProperty(LABEL).getValue();
+				int idInt = (int) id;
+				SimpleTokenizable t = new SimpleTokenizable(Integer.toUnsignedLong(idInt), string);
+				extTokenField.addTokenizable(t);
+
+				// if you would use a real container, you would filter the selected tokens out
+
+				// reset combobox
+				comboBox.setValue(null);
+			}
+		};
+	}
+
+	private void addValueAddedAndRemovedListeners(ExtTokenField extTokenField)
+	{
+		extTokenField.addTokenAddedListener(event -> Notification.show("Token added: " + event.getTokenizable().getStringValue()));
+		extTokenField.addTokenRemovedListener(event -> Notification.show("Token removed: " + event.getTokenizable().getStringValue()));
+	}
+
+	private void removeValueAddedAndRemovedListeners(ExtTokenField extTokenField)
+	{
+		Collection<?> listenerAdded = extTokenField.getListeners(TokenAddedEvent.class);
+		if (listenerAdded != null && !listenerAdded.isEmpty())
+		{
+			extTokenField.removeTokenAddedListener((TokenAddedListener) listenerAdded.iterator().next());
+		}
+
+		Collection<?> listenerRemoved = extTokenField.getListeners(TokenRemovedEvent.class);
+		if (listenerRemoved != null && !listenerRemoved.isEmpty())
+		{
+			extTokenField.removeTokenRemovedListener((TokenRemovedListener) listenerRemoved.iterator().next());
+		}
+	}
+
+	private class ConfigurableLayout extends VerticalLayout
+	{
+
+		private ExtTokenField	tokenField						= new ExtTokenField();
+		private CheckBox		delete							= new CheckBox("activate or deactivate delete action", true);
+		private CheckBox		comboBoxOrButton				= new CheckBox("ComboBox or Button");
+		private CheckBox		readOnly						= new CheckBox("read only");
+		private CheckBox		enabled							= new CheckBox("enabled", true);
+		private CheckBox		addCustomAction					= new CheckBox("add or remove custom action");
+		private CheckBox		readOnlyIgnoringCustomAction	= new CheckBox("should the custom action ignore read only");
+		private CheckBox		activateValueChangeListener		= new CheckBox("add or remove ValueChangeListener");
+
+		private ComboBox		comboBox						= TestUI.getComboBox();
+		private Button			addButton						= TestUI.getAddButton();
+
+		public ConfigurableLayout()
+		{
+			tokenField.setCaption("Tokens");
+			tokenField.setInputField(comboBox);
+			tokenField.setEnableDefaultDeleteTokenAction(delete.getValue());
+
+			List<SimpleTokenizable> list = new LinkedList<>();
+			for (int i = 0; i < LANGUAGES.length - 2; i++)
+			{
+				list.add(new SimpleTokenizable(i, LANGUAGES[i]));
+			}
+			tokenField.setValue(list);
+
+			comboBox.addValueChangeListener(getComboBoxValueChange(tokenField, comboBox));
+
+			FormLayout field = new FormLayout(tokenField);
+			field.setSizeFull();
+
+			addComponent(field);
+			FormLayout configLayout = new FormLayout(readOnly, enabled, delete, comboBoxOrButton, addCustomAction, readOnlyIgnoringCustomAction, activateValueChangeListener);
+			configLayout.setCaption("modify settings");
+			configLayout.setSizeFull();
+			addComponent(configLayout);
+			setupCheckBoxes();
+		}
+
+		private void setupCheckBoxes()
+		{
+			delete.addValueChangeListener(event -> tokenField.setEnableDefaultDeleteTokenAction(delete.getValue()));
+
+			readOnly.addValueChangeListener(event -> tokenField.setReadOnly(!tokenField.isReadOnly()));
+
+			enabled.addValueChangeListener(event -> tokenField.setEnabled(!tokenField.isEnabled()));
+
+			comboBoxOrButton.addValueChangeListener(event -> {
+				if (tokenField.hasInputField())
+					tokenField.setInputButton(addButton);
+				else
+					tokenField.setInputField(comboBox);
+			});
+
+			TokenizableAction tokenizableAction = new TokenizableAction("id1", FontAwesome.GEARS)
+			{
+
+				public void onClick(Tokenizable token)
+				{
+					Notification.show("clicked " + token.getStringValue());
+				};
+			};
+
+			addCustomAction.addValueChangeListener(event -> {
+				if (tokenField.hasTokenizableAction(tokenizableAction))
+				{
+					tokenField.removeTokenizableAction(tokenizableAction);
+				}
+				else
+				{
+					tokenizableAction.setInheritsReadOnlyAndEnabled(!readOnlyIgnoringCustomAction.getValue());
+					tokenField.addTokenAction(tokenizableAction);
+				}
+			});
+
+			readOnlyIgnoringCustomAction.addValueChangeListener(event -> {
+				if (tokenField.hasTokenizableAction(tokenizableAction))
+				{
+					tokenField.removeTokenizableAction(tokenizableAction);
+					tokenizableAction.setInheritsReadOnlyAndEnabled(!readOnlyIgnoringCustomAction.getValue());
+					tokenField.addTokenAction(tokenizableAction);
+				}
+			});
+
+			activateValueChangeListener.addValueChangeListener(event -> {
+				if (tokenField.getListeners(TokenAddedEvent.class).isEmpty())
+				{
+					addValueAddedAndRemovedListeners(tokenField);
+				}
+				else
+				{
+					removeValueAddedAndRemovedListeners(tokenField);
+				}
+			});
+		}
+	}
+}
